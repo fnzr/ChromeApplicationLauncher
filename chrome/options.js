b64fdcc73fd29030ff1bae29249c17a5797d4c4a
MEGAZORD <sazuna@users.noreply.github.com>
2018-03-18 18:03:45 -0300

    UI support for multiple launchers

--- chrome/options.js
+++ chrome/options.js
@@ -1,38 +1,72 @@
 function save_options() {
-    var els = document.getElementsByClassName("argument");
-    var args = [];
-    for(var i=0; i< els.length; i++){
-        var value = els.item(i).value;
-        if(value !== "") args.push(value);
+    let status = document.getElementById('status');
+
+    let path = document.getElementById('path').value;
+    if (path === "") {
+        status.textContent = "Must provide a path!";
     }
-    chrome.storage.sync.set({
-        path: document.getElementById('path').value,
-        arguments: args
-    }, function () {
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
+    let name = document.getElementById("name").value;
+    if (name === "") {
+        name = path.split("\\").pop();
+    }
+    let els = document.getElementsByClassName("argument");
+    let args = [];
+    for (let i = 0; i < els.length; i++) {
+        let value = els.item(i).value;
+        if (value !== "") args.push(value);
+    }
+    chrome.storage.sync.get({launchers: {}}, function (items) {
+        let launchers = items.launchers;
+        launchers[name] = {
+            path: document.getElementById('path').value,
+            arguments: args
+        };
+        chrome.storage.sync.set({launchers: launchers},
+            function () {
+                status.textContent = 'Options saved.';
+                setTimeout(function () {
+                    status.textContent = '';
+                }, 750);
+            });
     });
 }
 
 function restore_options() {
-    chrome.storage.sync.get({path: "", arguments: []}, function (items) {
-        document.getElementById('path').value = items.path;
-        items.arguments.forEach(function(arg){
-            if(arg !== "") add_argument(arg);
-        })
+    chrome.storage.sync.get({launchers: {}}, function (items) {
+        let options = document.getElementById("names")
+        let launcher;
+        for (launcher in items.launchers) {
+            let opt = document.createElement("OPTION");
+            opt.textContent = launcher;
+            opt.value = launcher;
+            options.appendChild(opt);
+        }
+    });
+}
+
+function fill_launcher() {
+    let args = document.getElementById("args");
+    while (args.firstChild) {
+        args.removeChild(args.firstChild);
+    }
+    let name = document.getElementById("names").value;
+    chrome.storage.sync.get({launchers: {}, lastUsed: ""}, function (items) {
+        let launcher = items.launchers[name];
+        document.getElementById("name").value = name;
+        document.getElementById('path').value = launcher.path;
+        launcher.arguments.forEach(function (arg) {
+            if (arg !== "") add_argument(arg);
+        });
     });
 }
 
 function add_argument(value) {
-    var root = document.createElement('div');
-    var input = document.createElement('input');
+    let root = document.createElement('div');
+    let input = document.createElement('input');
     input.style.width = "300px";
     input.type = "text";
     input.className = "argument";
-    if(typeof value === "string"){
+    if (typeof value === "string") {
         input.value = value;
     }
     root.appendChild(input);
@@ -42,3 +76,4 @@ function add_argument(value) {
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('add').addEventListener('click', add_argument);
+document.getElementById("names").addEventListener('change', fill_launcher)
