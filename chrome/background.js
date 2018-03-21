a71a1d030b23af2289a3510ca0c07bfdd68c88b5
MEGAZORD <sazuna@users.noreply.github.com>
2018-03-20 22:46:16 -0300

    Progress on UI updates support for script feedback

--- chrome/background.js
+++ chrome/background.js
@@ -3,23 +3,45 @@ const app_name = 'fmatos.chromeapplicationlauncher';
 function start(name, tab) {
     chrome.storage.sync.get({launchers: {}}, function (items) {
         let launcher = items.launchers[name];
-        let port = chrome.runtime.connectNative(app_name);
-        let id = Date.now();
-        port.onMessage.addListener(function (msg) {
-            chrome.runtime.sendMessage("", {request: "message_received", id: id, message: msg});
-        });
-        port.onDisconnect.addListener(function () {
-            chrome.runtime.sendMessage("", {request: "task_end", id: id});
-            console.log("Disconnected");
+        chrome.storage.sync.set({lastUsed: name});
+
+        chrome.storage.local.get('tasks', function(local){
+            let tasks = local.tasks === undefined ? {} : local.tasks;
+
+            let port = chrome.runtime.connectNative(app_name);
+            let id = Date.now();
+            tasks[id] = {
+                id: id,
+                last_message: "Task started",
+                finished: false,
+                title: tab.title,
+                url: tab.url
+            };
+
+            port.onMessage.addListener(function (msg) {
+                tasks[id].last_message = msg.data;
+                chrome.storage.local.set({tasks: tasks, last_updated_task: id});
+                //chrome.runtime.sendMessage("", {request: "message_received", id: id, message: msg});
+            });
+            port.onDisconnect.addListener(function () {
+                tasks[id].finished = true;
+                chrome.storage.local.set({tasks: tasks, last_updated_task: id});
+                //chrome.runtime.sendMessage("", {request: "task_end", id: id});
+                console.log("Disconnected");
+            });
+
+            let args = "";
+            launcher.arguments.forEach(function (arg) {
+                args += arg + " ";
+            });
+            args = args.replace("$url", tab.url);
+            //console.log({exec: launcher.path, args: args});
+            port.postMessage({exec: launcher.path, args: args});
+
+            //chrome.runtime.sendMessage("", {request: "task_begin", id: id, tab: tab});
+            chrome.storage.local.set({tasks: tasks, last_updated_task: id});
         });
 
-        let args = "";
-        launcher.arguments.forEach(function (arg) {
-            args += arg + " ";
-        });
-        args = args.replace("$url", tab.url);
-        port.postMessage({exec: launcher.path, args: args});
-        chrome.runtime.sendMessage("", {request: "task_begin", id: id, tab: tab});
     });
 }
 
@@ -32,9 +54,11 @@ chrome.runtime.onMessage.addListener((message) => {
     }
 });
 
+/*
 chrome.browserAction.onClicked.addListener(function (tab, ev) {
     console.log(tab)
     console.log(ev)
     start(tab.url)
 
 });
+*/
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,46 +1,93 @@
-let tasks = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("launch").addEventListener('click', launch);
-    chrome.storage.sync.get({launchers: {}, lastUsed: ""}, function (items) {
+    chrome.storage.sync.get({launchers: {}, lastUsed: "", tasks: ""}, function (items) {
         let options = document.getElementById("launchers");
         let launcher;
         for (launcher in items.launchers) {
-            let opt = document.createElement("OPTION");
+            let opt = document.createElement("option");
             opt.textContent = launcher;
             opt.value = launcher;
+            if (launcher === items.lastUsed) {
+                opt.selected = true;
+            }
             options.appendChild(opt);
         }
+        chrome.storage.local.get({tasks: {}}, function (local) {
+            console.log(local);
+            for (key in local.tasks) {
+                create_task(local.tasks[key]);
+            }
+        })
     });
+
 });
 
+function create_task(task) {
+    let classes =  task.finished ? "text-white bg-success" : "text-white bg-info";
+
+    let html =
+        '<li class="list-group-item" style="padding: 0px;">' +
+        '<div id="div-' + task.id + '" class="'+ classes +'">' +
+        '    <p>' + task.title + '</p>' +
+        '    <p>' + task.url + '</p>' +
+        '    <p id="p-' + task.id + '">' + task.last_message + '</p>' +
+        '</div></li>';
+    let temp = document.createElement('div');
+    temp.innerHTML = html;
+    let ul = document.getElementById("task-list");
+    ul.insertBefore(temp.firstChild, ul.firstChild);
+}
+
+chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === "local") {
+        if ("tasks" in changes) {
+            chrome.storage.local.get('last_updated_task', function(item){
+                let id = item.last_updated_task;
+                let task = changes["tasks"].newValue[id];
+                if(document.getElementById('div-' + id) === null){
+                    create_task(task);
+                }
+                let p = document.getElementById('p-' + id);
+                p.innerHTML = task.last_message;
+
+                if (task.finished) {
+                    let div = document.getElementById('div-' + task.id);
+                    div.className = "text-white bg-success";
+                }
+            });
+        }
+    }
+});
+
+/*
 chrome.runtime.onMessage.addListener((message) => {
     if (message.request === "task_begin") {
         let html =
             '<li class="list-group-item" style="padding: 0px;">' +
-            '<div id="div-'+ message.id +'" class="text-white bg-info">' +
-            '    <h4>'+message.tab.title+'</h4>' +
-            '    <p>'+ message.tab.url +'</p>' +
-            '    <p id=p-"' +message.id +'">Task started...</p>' +
+            '<div id="div-' + message.id + '" class="text-white bg-info">' +
+            '    <h4>' + message.tab.title + '</h4>' +
+            '    <p>' + message.tab.url + '</p>' +
+            '    <p id=p-"' + message.id + '">Task started...</p>' +
             '</div></li>';
         let temp = document.createElement('div');
         temp.innerHTML = html;
         let ul = document.getElementById("task-list");
-        ul.insertBefore(html, ul.firstChild);
+        ul.insertBefore(temp.firstChild, ul.firstChild);
     }
-    else if(message.request === "message_received"){
+    else if (message.request === "message_received") {
         let p = document.getElementById('p-' + message.id);
         p.innerText = message.message;
     }
-    else if(message.request === "task_end"){
+    else if (message.request === "task_end") {
         let div = document.getElementById('div-' + message.id);
         div.className = "text-white bg-success";
 
         let p = document.getElementById('p-' + message.id);
-        p.innerText = "Task Finished!";
+        p.innerHTML = "Task Finished!";
     }
 });
-
+*/
 function launch() {
     console.log("launching");
     chrome.runtime.sendMessage("", {
